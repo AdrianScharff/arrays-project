ce1aa5ccca7a79468f30f1dc796b813f5b305482
AdrianScharff <adrian18juarez@gmail.com>
2023-09-25 21:47:07 -0600

    Finish array project

---
+++ app.js
@@ -0,0 +1,24 @@
+/* const emptyArray = [];
+
+for(let i = 0; i < 10; i ++) {
+    let random = Math.floor(Math.random() * 100);
+    emptyArray.push(random);
+}
+
+console.log(emptyArray);
+
+const input = prompt('Ingresa varias palabras separadas por coma, todas las que quieras');
+const arr = input.split(",");
+
+console.log(arr); */
+
+const uArray = [10,40,30,20,15,5];
+uArray.sort((a, b) => a - b);
+
+console.log(uArray);
+
+const min = Math.min(...uArray);
+const max = Math.max(...uArray);
+
+console.log('The minimum value in the array is: ' + min);
+console.log('The maximum value in the array is: ' + max);
